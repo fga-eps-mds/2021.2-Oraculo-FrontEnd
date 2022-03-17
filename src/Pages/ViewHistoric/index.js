5fdbd5638f3c3cf263fa8c48a47ca0549c1506ea
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-03-17 17:18:31 -0300

    refatorando nome de variáveis e dados do histórico
    Co-authored-by: Pedro Victor Lima Torreão <B3holder2@users.noreply.github.com>

--- src/Pages/ViewHistoric/index.js
+++ src/Pages/ViewHistoric/index.js
@@ -1,54 +1,95 @@
 import ForwardSector from "../../Components/ForwardSector";
 import React, {  useState , useEffect} from "react";
 import { useParams } from "react-router";
+//import ReactDOM from "react-dom";
+//import { matchPath } from "react-router-dom"
 import SectionMovBlock from "../../Components/SectionMovBlock";
-import { getRecordHistory } from "../../Services/Axios/processService";
+import { getRecordHistory , getUserByEmail} from "../../Services/Axios/processService";
 import toast from "react-hot-toast";
+import {
+    getDepartments,
+    getInfoUser,
+} from "../../Services/Axios/profileService";
 
-function ViewHistoric(props) {
-    const [forward, setForward] = useState([]);
-    const  id  = useParams();// TOdo como usar
+function ViewHistoric() {
+    const [forwardHistory, setForwardHistory] = useState([]);
+    
+    //function BlogPost() {
+        // Desconstruímos da URL a varíavel idPost
+    //    const { idPost } = useParams();
+    //    return <div>ID do post: {idPost}</div>;
+    //  }
+    const  {id}  = useParams();
+    console.log(id)
+    
+    // {
+    //     const { location } = useRouter()
+    //     const { pathname } = location
+      
+    //     const pattern = `(.*)?${path}`
+    //     const match = matchPath(pathname, { path: pattern }) || {}
+      
+    //    return match.params
+    //  };
 
     useEffect(() => {
-        async function fetchHistoryData(){
-            const responseHR = await getRecordHistory(toast, 1);
-            
-            console.log(responseHR);
-
-            const arrInfoForward = await Promise.all(
-                responseHR.map((res) => organizeHistoric(res))
+        // deve salvar historico no forwardHistory 
+        async function saveHistoryData(){
+            const responseHistory = await getRecordHistory(toast, 1);
+            //console.log(responseHistory);//! remove
+            const arrayForwardHistory = await Promise.all(
+                responseHistory.map((history) => organizeHistory(history))
             );
-            
-    
-            await setForward(arrInfoForward);
+            setForwardHistory(arrayForwardHistory);
         }
+        
+        // deve organizar um historico para seu respectivo formato
+        async function organizeHistory(history){
 
-        async function organizeHistoric(res){
-            var newForward = []
-            const dataFormatadaCreatedAt = formatedDate(res.createdAt);
-            const dataFormatadaUpdatedAt = formatedDate(res.updatedAt);
+            var newForwardHistory = []
+            const email = history.forwarded_by;
+            console.log(email);
+            // caso seja 
+            if(email != null){
+                const originSecID = history.origin_id;
+                
+                const destinationID = history.destination_id;
+                // Todo: remover req daqui
+                const allDepartments = await getDepartments();
+                console.log(allDepartments)
 
-            newForward = {
-                setor: "",
-                setorOrigin:"",
-                date: dataFormatadaCreatedAt,
-                dateForward: dataFormatadaUpdatedAt,
-                name: "",
-            };
-            
-            return newForward;
+                const setor = allDepartments.filter((indice) => {
+                    return indice.id === destinationID;
+                });
+                const originSection = allDepartments.filter((indice) => {
+                    return indice.id === originSecID;
+                });
+
+                const user = await getUserByEmail(email);
+                
+                
+                newForwardHistory = {
+                    setor: setor[0].name, 
+                    setorOrigin:  originSection[0].name, 
+                    date: formatedDate(history.createdAt),
+                    dateForward: formatedDate(history.updatedAt),
+                    name: user.name,
+                };
+                
+            }
+            return newForwardHistory;
         };
-        fetchHistoryData();
-
+        saveHistoryData();
+        // ? deve estar aqui?
         const getDate = () => {
             var data = new Date();
             var dia = String(data.getDate()).padStart(2, "0");
             var mes = String(data.getMonth() + 1).padStart(2, "0");
             var ano = data.getFullYear();
             return dia + "/" + mes + "/" + ano;
-          };
+        };
     });
-
+    
     const formatedDate = (infoDate) => {
         const dataDone = new Date(infoDate);
         return (
@@ -63,9 +104,7 @@ function ViewHistoric(props) {
     
     return (
         <div>
-            
-            <ForwardSector forward={forward}/>
-
+            <ForwardSector forward={forwardHistory}/>
         </div>
         
     )
