4de076f5aa31ad6c9554a3e2bc8f70a2354d5372
gabriel <gabrielribeirof69@gmail.com>
2022-04-06 21:55:27 -0300

    testing if components is rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 import MainButton from "./Components/MainButton";
 import HeaderWithButtons from "./Components/HeaderWithButtons";
+import { StyledAlertDialog, TagList } from "./Components/AddTagDialog/styles";
+import GenericBlueButton from "./Components/GenericBlueButton";
+import { Checkbox, Modal } from "antd";
+import GenericWhiteButton from "./Components/GenericWhiteButton";
+import { StyledBlueRectangle, StyledWhiteRectangle } from "./Pages/CreateRecord/styles";
+import { StyledListGroup } from "./Components/Departments/style";
+
 
 test("renders main app component", () => {
   render(<App />);
@@ -13,4 +20,40 @@ test("renders main button component", () => {
 
 test("renders header with buttons component", () => {
   render(<HeaderWithButtons/>);
+});
+
+test("renders styyledalert buttons component", () => {
+  render(<StyledAlertDialog/>)
+});
+
+test("renders genericbluebutton buttons component", () => {
+  render(<GenericBlueButton/>)
+});
+
+test("renders genericWhiteButton compenent", () => {
+  render(<GenericWhiteButton/>)
+});
+
+test("renders modal component", () => {
+  render(<Modal/>)
+});
+
+test("renders checkbox compenent", () => {
+  render(<Checkbox/>)
+});
+
+test("renders TagList compenent", () => {
+  render(<TagList/>)
+});
+
+test("renders styledbluerectangle compenent", () => {
+  render(<StyledBlueRectangle/>)
+});
+
+test("renders styledwhiterectangle compenent", () => {
+  render(<StyledWhiteRectangle/>)
+});
+
+test("renders styledListGroup compenent", () => {
+  render(<StyledListGroup/>)
 });
