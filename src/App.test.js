e18ad535c6119a7c4e9d9730fed90be990a27746
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-04-06 21:37:22 -0300

    feat: creating department test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 import MainButton from "./Components/MainButton";
 import HeaderWithButtons from "./Components/HeaderWithButtons";
+import CreateDepartment from "./Pages/CreateDepartment";
 
 test("renders main app component", () => {
   render(<App />);
@@ -14,3 +16,20 @@ test("renders main button component", () => {
 test("renders header with buttons component", () => {
   render(<HeaderWithButtons/>);
 });
+
+describe("CreateDepartment test", () => {
+  test("Should render  Create Department page",async  () => {
+    render(<CreateDepartment/>)
+    const cadastrarButton = screen.getByText("Cadastrar")
+    const voltarButton = screen.getByText("Voltar")
+    const departamentoField = screen.getByPlaceholderText("Departamento")
+    
+    userEvent.paste(departamentoField, "teste")
+
+    expect(departamentoField).toHaveValue('teste')
+    
+    userEvent.click(cadastrarButton)
+    userEvent.click(voltarButton)
+  });
+  
+})
