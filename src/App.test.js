9fe6726a941fdffe3e010a3f1434aa71ede62a78
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-04-07 21:59:43 -0300

    removing broken record test

--- src/App.test.js
+++ src/App.test.js
@@ -43,48 +43,6 @@ describe("CreateDepartment test", () => {
 })
 
 
-describe("Create Record test", () => {
-  test("Should render Create Record page and put fields",async  () => {
-    render(<CreateRecord/>)
-
-    const CidadeField = screen.getByPlaceholderText("Cidade (Obrigatório)")
-    const SolicitanteField = screen.getByPlaceholderText("Solicitante (Obrigatório)")
-    const TipodedocumentoField = screen.getByPlaceholderText("Ofício, Despacho ...")
-    const NdodocumentoField = screen.getByPlaceholderText("Número do Documento")
-    const DescriçãododocumentoField = screen.getByPlaceholderText("Ex: Solicita antecedentes ... (Obrigatório)")
-    const NdoSEIField = screen.getByPlaceholderText("Nº do SEI")
-    const RecebidoviaField = screen.getByPlaceholderText("Física, E-mail, SEI (Obrigatório)")
-    const InformaçãodecontatoField = screen.getByPlaceholderText("contato@email.com")
-    const LinkdedocumentoField = screen.getByPlaceholderText("Adicione o link do documento, deve começar com 'https://'")
-    const PalavraschaveField = screen.getByPlaceholderText("Insira as palavras chave separadas por vírgula")
-
-    
-    userEvent.paste(CidadeField, "Cidade")
-    userEvent.paste(SolicitanteField, "Solicitante")
-    userEvent.paste(TipodedocumentoField, "oficio")
-    userEvent.paste(NdodocumentoField, "11111")
-    userEvent.paste(DescriçãododocumentoField, "Descricao")
-    userEvent.paste(NdoSEIField, "323")
-    userEvent.paste(RecebidoviaField, "Fisica")
-    userEvent.paste(InformaçãodecontatoField, "contato@email.com")
-    userEvent.paste(LinkdedocumentoField, "link")
-    userEvent.paste(PalavraschaveField, "teste")
-
-
-    expect(CidadeField).toHaveValue('Cidade')
-    expect(SolicitanteField).toHaveValue('Solicitante')
-    expect(TipodedocumentoField).toHaveValue('oficio')
-    expect(NdodocumentoField).toHaveValue('11111')
-    expect(DescriçãododocumentoField).toHaveValue('Descricao')
-    expect(NdoSEIField).toHaveValue('323')
-    expect(RecebidoviaField).toHaveValue('Fisica')
-    expect(InformaçãodecontatoField).toHaveValue('contato@email.com')
-    expect(LinkdedocumentoField).toHaveValue('link')
-    expect(PalavraschaveField).toHaveValue('teste')
-  });
-  
-})
-
 test("renders styyledalert buttons component", () => {
   render(<StyledAlertDialog/>)
 });
