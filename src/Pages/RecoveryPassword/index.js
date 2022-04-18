7c6324f0c1dafe022d905dbb561f51cec49aa06b
ciceroff <cicero.barrozo13@gmail.com>
2022-04-18 18:50:46 -0300

    fix: fix password recovery file names
    Co-authored-by: Gabriel De Souza <Gabrielribeirof19@users.noreply.github.com>

--- src/Pages/RecoveryPassword/index.js
+++ src/Pages/RecoveryPassword/index.js
@@ -1,92 +1,90 @@
-import React from "react";
-import { FaLock, FaUserCircle } from "react-icons/fa";
-import toast, { Toaster } from "react-hot-toast";
-import Logo from "../../Assets/logo-dark.svg";
-import Header from "../../Components/Header";
-import LoginInput from "../../Components/LoginInput/index";
-import MainButton from "../../Components/MainButton";
-import { FormLogin, StyledDiv } from "./styles";
-import jwt_decode from "jwt-decode";
+import React from 'react'
+import { FaLock, FaUserCircle } from 'react-icons/fa'
+import toast, { Toaster } from 'react-hot-toast'
+import Logo from '../../Assets/logo-dark.svg'
+import Header from '../../Components/Header'
+import LoginInput from '../../Components/LoginInput/index'
+import MainButton from '../../Components/MainButton'
+import { FormLogin, StyledDiv } from './styles'
+import jwt_decode from 'jwt-decode'
 import {
   getInfoUser,
   getToken,
-  sendEmailToRecoverPassword,
+  passwordRecovery,
   verifyToken,
-} from "../../Services/Axios/profileService";
-import { isAuthenticated, login } from "../../Auth/Auth";
-import { withRouter } from "react-router";
-import { Link } from "react-router-dom";
+} from '../../Services/Axios/profileService'
+import { isAuthenticated, login } from '../../Auth/Auth'
+import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
 
-class RecoveryPassword extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            token: '',
-            password: ''
-        };
+class RecoveryPassword extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: '',
+      password: '',
     }
+  }
 
-    async handleClick(event) {
-        const passwordReq = {token: this.state.token, password: this.state.password} ;
-        const result = await passwordRecovery(passwordReq, toast);
-        if(result){
-            this.props.history.push("/login")
-        }
-      }
-
-    setToken(newToken) {
-        this.setState({ token: newToken });
-      }
-
-    setPassword(newPassword) {
-        this.setState({ password: newPassword });
+  async handleClick(event) {
+    const passwordReq = {
+      token: this.state.token,
+      password: this.state.password,
     }
-
-    render (){
-        return (
-            <>
-            <Header />
-            <FormLogin>
-                <form>
-                    <img src={Logo} alt="Logo" />
-                    <LoginInput
-                    idInput="token"
-                    nameInput="token"
-                    placeholderInput="Digite o token enviado no email"
-                    inputType="token"
-                    valueInput={this.state.token}
-                    onChangeInput={(event) => this.setToken(event.target.value)}
-                    >
-                    <FaUserCircle />
-                
-                    </LoginInput>
-                    <LoginInput
-                    idInput="password"
-                    nameInput="password"
-                    placeholderInput="Digite a nova senha"
-                    inputType="password"
-                    valueInput={this.state.password}
-                    onChangeInput={(event) => this.setPassword(event.target.value)}
-                    >
-                    <FaUserCircle />
-                
-                    </LoginInput>
-                    <StyledDiv>
-                        <MainButton
-                            onClick={(event) => {
-                                this.handleClick(event);
-                            }}
-                            title={"Enviar"}
-                        />
-                    </StyledDiv>
-            </form>
-            </FormLogin>
-                
-            </>
-          
-        );
+    const result = await passwordRecovery(passwordReq, toast)
+    if (result) {
+      this.props.history.push('/login')
     }
+  }
+
+  setToken(newToken) {
+    this.setState({ token: newToken })
+  }
+
+  setPassword(newPassword) {
+    this.setState({ password: newPassword })
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <FormLogin>
+          <form>
+            <img src={Logo} alt="Logo" />
+            <LoginInput
+              idInput="token"
+              nameInput="token"
+              placeholderInput="Digite o token enviado no email"
+              inputType="token"
+              valueInput={this.state.token}
+              onChangeInput={(event) => this.setToken(event.target.value)}
+            >
+              <FaUserCircle />
+            </LoginInput>
+            <LoginInput
+              idInput="password"
+              nameInput="password"
+              placeholderInput="Digite a nova senha"
+              inputType="password"
+              valueInput={this.state.password}
+              onChangeInput={(event) => this.setPassword(event.target.value)}
+            >
+              <FaUserCircle />
+            </LoginInput>
+            <StyledDiv>
+              <MainButton
+                onClick={(event) => {
+                  this.handleClick(event)
+                }}
+                title={'Enviar'}
+              />
+            </StyledDiv>
+          </form>
+        </FormLogin>
+      </>
+    )
+  }
 }
-
 
 export default withRouter(RecoveryPassword)
