ea5e6073ac816c041f4d8a493ad0ce94f6bb5052
ciceroff <cicero.barrozo13@gmail.com>
2022-04-18 22:41:07 -0300

    refactor: remove unused code

--- src/Pages/ResetPassword/index.js
+++ src/Pages/ResetPassword/index.js
@@ -1,76 +1,63 @@
 import React from "react";
-import { FaLock, FaUserCircle } from "react-icons/fa";
-import toast, { Toaster } from "react-hot-toast";
+import { FaUserCircle } from "react-icons/fa";
+import toast from "react-hot-toast";
 import Logo from "../../Assets/logo-dark.svg";
 import Header from "../../Components/Header";
 import LoginInput from "../../Components/LoginInput/index";
 import MainButton from "../../Components/MainButton";
 import { FormLogin, StyledDiv } from "./styles";
-import jwt_decode from "jwt-decode";
-import {
-  getInfoUser,
-  getToken,
-  sendEmailToRecoverPassword,
-  verifyToken,
-} from "../../Services/Axios/profileService";
-import { isAuthenticated, login } from "../../Auth/Auth";
+import { sendEmailToRecoverPassword } from "../../Services/Axios/profileService";
 import { withRouter } from "react-router";
-import { Link } from "react-router-dom";
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+    };
+  }
 
-class ResetPassword extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            email: ''
-        };
+  async handleClick() {
+    const email = this.state.email;
+    const result = await sendEmailToRecoverPassword(email, toast);
+    if (result) {
+      this.props.history.push("/recuperar-senha");
     }
+  }
 
-    async handleClick(event) {
-        const email = this.state.email ;
-        const result = await sendEmailToRecoverPassword(email, toast);
-        if(result){
-            this.props.history.push("/recuperar-senha")
-        }
-      }
+  setEmail(newMail) {
+    this.setState({ email: newMail });
+  }
 
-    setEmail(newMail) {
-        this.setState({ email: newMail });
-      }
-
-    render (){
-        return (
-            <>
-            <Header />
-            <FormLogin>
-                <form>
-                    <img src={Logo} alt="Logo" />
-                    <LoginInput
-                    idInput="email"
-                    nameInput="email"
-                    placeholderInput="Digite seu e-mail cadastrado no sistema"
-                    inputType="email"
-                    valueInput={this.state.email}
-                    onChangeInput={(event) => this.setEmail(event.target.value)}
-                    >
-                    <FaUserCircle />
-                
-                    </LoginInput>
-                    <StyledDiv>
-                        <MainButton
-                            onClick={(event) => {
-                                this.handleClick(event);
-                            }}
-                            title={"Enviar"}
-                        />
-                    </StyledDiv>
-            </form>
-            </FormLogin>
-                
-            </>
-          
-        );
-    }
+  render() {
+    return (
+      <>
+        <Header />
+        <FormLogin>
+          <form>
+            <img src={Logo} alt="Logo" />
+            <LoginInput
+              idInput="email"
+              nameInput="email"
+              placeholderInput="Digite seu e-mail cadastrado no sistema"
+              inputType="email"
+              valueInput={this.state.email}
+              onChangeInput={(event) => this.setEmail(event.target.value)}
+            >
+              <FaUserCircle />
+            </LoginInput>
+            <StyledDiv>
+              <MainButton
+                onClick={(event) => {
+                  this.handleClick(event);
+                }}
+                title={"Enviar"}
+              />
+            </StyledDiv>
+          </form>
+        </FormLogin>
+      </>
+    );
+  }
 }
 
-
-export default withRouter(ResetPassword)
+export default withRouter(ResetPassword);
