d710de4931193d87114cae9e46b941b250fd6e6c
Andre Alves <andremralves@gmail.com>
2022-04-21 08:15:34 -0300

    add clock icon to show deadline of each record

--- src/Components/PocketDocument/index.js
+++ src/Components/PocketDocument/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { TagsList } from "../../Pages/ViewRecord/tags";
 import { getRecordTagColors } from "../../Services/Axios/tagsService";
+import { BsClockHistory } from "react-icons/bs";
 
 import { StyledBigDiv } from "./styles";
 
@@ -12,9 +13,13 @@ const PocketDocument = ({
   inclusionDate,
   seiNumber,
   registerId,
+  situation,
+  deadline,
 }) => {
   const [tags, setTags] = useState([]);
   const [hasTags, sethasTags] = useState(false);
+  const [strDifferenceTime, setStrDifferenceTime] = useState("");
+  const [msDifferenceTime] = useState(Date.parse(deadline) - Date.now());
 
   useEffect(() => {
     async function fetchTags() {
@@ -25,12 +30,35 @@ const PocketDocument = ({
       }
     }
 
+    setStrDifferenceTime(msToTime(msDifferenceTime));
+
     fetchTags();
   }, [registerId]);
 
   useEffect(() => {
     sethasTags(true);
   }, [tags]);
+
+  function msToTime(ms) {
+    let seconds = (ms / 1000).toFixed(1);
+    let minutes = (ms / (1000 * 60)).toFixed(1);
+    let hours = (ms / (1000 * 60 * 60)).toFixed(1);
+    let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+    if (seconds < 60) return seconds + " Sec";
+    else if (minutes < 60) return minutes + " Min";
+    else if (hours < 24) return hours + " Hrs";
+    else return days + " Dias";
+  }
+
+  const compareDate = () => {
+    const day = 24 * 60 * 60 * 1000;
+    if (msDifferenceTime < day)
+      return <BsClockHistory style={{ color: "red" }} size={40} />;
+    if (msDifferenceTime < 3 * day && msDifferenceTime > day)
+      return <BsClockHistory style={{ color: "orange" }} size={40} />;
+    if (msDifferenceTime > 3 * day)
+      return <BsClockHistory style={{ color: "green" }} size={40} />;
+  };
 
   return (
     <StyledBigDiv>
@@ -77,6 +105,25 @@ const PocketDocument = ({
             )
           );
         })
+      )}
+      {deadline && situation == "pending" && (
+        <a
+          href={`/ver-registro/${registerId}`}
+          style={{
+            width: "2.5rem",
+            height: "2.5rem",
+            margin: "1rem 0.5rem",
+          }}
+          className="tooltip"
+        >
+          {compareDate()}
+          <span className="tooltiptext">
+            <p>
+              Faltam: {strDifferenceTime} Prazo:{" "}
+              {new Date(deadline).toLocaleDateString("pt-br")}
+            </p>
+          </span>
+        </a>
       )}
     </StyledBigDiv>
   );
