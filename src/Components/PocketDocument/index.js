68e1a9d23692e3ca27dd31eb236d7275f671f9b5
Andre Alves <andremralves@gmail.com>
2022-04-21 08:51:31 -0300

    change tooltip text

--- src/Components/PocketDocument/index.js
+++ src/Components/PocketDocument/index.js
@@ -40,14 +40,11 @@ const PocketDocument = ({
   }, [tags]);
 
   function msToTime(ms) {
-    let seconds = (ms / 1000).toFixed(1);
-    let minutes = (ms / (1000 * 60)).toFixed(1);
     let hours = (ms / (1000 * 60 * 60)).toFixed(1);
     let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
-    if (seconds < 60) return seconds + " Sec";
-    else if (minutes < 60) return minutes + " Min";
-    else if (hours < 24) return hours + " Hrs";
-    else return days + " Dias";
+    if (hours < 0) return "Prazo Vencido";
+    else if (hours < 24) return "HOJE!";
+    else return "Faltam " + Math.ceil(days) + " Dias";
   }
 
   const compareDate = () => {
@@ -119,8 +116,9 @@ const PocketDocument = ({
           {compareDate()}
           <span className="tooltiptext">
             <p>
-              Faltam: {strDifferenceTime} Prazo:{" "}
-              {new Date(deadline).toLocaleDateString("pt-br")}
+              {strDifferenceTime}
+              <br />
+              Prazo: {new Date(deadline).toLocaleDateString("pt-br")}
             </p>
           </span>
         </a>
